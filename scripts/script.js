6e8cce50171f3ffac5f2ea589273de75ea2b304d
Alex Junior <alexjr2oficial@gmail.com>
2023-01-28 14:44:38 -0300

    melhorando a responsividade

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 alert("Portfolio em desenvolvimento!!");
+
+const btnMenu = document.getElementById("btn-menu");
+const navegation = document.getElementById("navegation");
+
+btnMenu.addEventListener("click", () => {
+  navegation.classList.toggle("active");
+});
