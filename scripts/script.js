a77e7730ff19d684e43d1b3bfb7850bd80da2933
Alex Junior <alexjr2oficial@gmail.com>
2023-01-30 11:16:22 -0300

    organizando as css em pastas

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-alert("Portfolio em desenvolvimento!!");
+// alert("Portfolio em desenvolvimento!!");
 
 const btnMenu = document.getElementById("btn-menu");
 const navegation = document.getElementById("navegation");
