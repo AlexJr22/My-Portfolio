2d05e7e4af665436cbd6d5de7054f4658321d015
Alex Junior <alexjr2oficial@gmail.com>
2023-02-01 12:39:18 -0300

    gerando os icones de forma dinamica

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,26 @@
 alert("Portfolio em desenvolvimento!!");
 
-const btnMenu = document.getElementById("btn-menu");
-const navegation = document.getElementById("navegation");
+const btnMenu = document.getElementById("btn-menu"); // botão do menu
+const navegation = document.getElementById("navegation"); // elemento guarda os link âncoras
 
+const containerTech = document.getElementById("containerTech"); // container que guarda os icones
+
+// adicionado um evento de click no btnMenu
 btnMenu.addEventListener("click", () => {
   navegation.classList.toggle("active");
 });
+
+// gerando de forma dinâmica os icones
+fetch("./techs.json")
+  .then((response) => response.json())
+  .then((techs) =>
+    techs.forEach(({ icon, tech }) => {
+      containerTech.innerHTML += `
+        <div class="card">
+          <img class="card-img" src="assets/icons/${icon}" alt="${tech}" />
+
+          <span class="name-icon">${tech}</span>
+        </div>
+      `;
+    })
+  );
